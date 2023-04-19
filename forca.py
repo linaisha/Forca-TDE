2702e3a17d8bd453483efb8abbb08e556613234f
ElviajeroO <130417561+ElviajeroO@users.noreply.github.com>
2023-04-19 13:58:30 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -50,57 +50,48 @@ while op == 1:
     else:
         print('erro')
     
-    chances = 6
-
     oculta = [x for x in palavra]
+
+    tam = len(oculta)
     
     print(oculta)
 
-    adivinhando = ""
-    # começamos de fato o codigo
-    while adivinhando != palavra or chances <= 0:
-        # usuario digita uma letra
-        letra = str(input("Digite uma letra: "))
-        # não queremos que o usuario digite mais de uma letra
-        if len(letra) > 1:
-            print("Apenas UMA letra, seu vacilao!")
-            #continue
-        # incluir a letra digitada na lista
-        digitadas.append(letra)
-        # variavel da palavra que a pessoa esta adibinhando
-        adivinhando = ""
-        # aqui colocamos que se a pessoa acertar alguma letra, a mesma vai aparecer, senão ela mantem um _ no lugar
-        for letrasegredo in palavra:
-            if letrasegredo in digitadas:
-                adivinhando += letrasegredo
-            else:
-                adivinhando += "_"
-        # se errar a letra as chances diminuem em um
-        if letra not in palavra:
-            chances -= 1
-            print("Você errou uma letra parceiro, que pena... -1 vida")
+    P = 0
 
-        # se as chances acabarem o jogo acaba com derrota
-        if chances == 0:
-            print("Vish, perdeu meu amigo! Foi enforcado!")
-            V = 0
+    adivinhando = []
 
+    while(P < tam):
+        adivinhando.append('-')
+        P += 1
+
+    chances = 0
+
+    while chances < 6:
+        letra = str(input('Digite uma letra:'))
+        
+        P = 0
+        Flag = 0
+        while P < tam:
+
+            if oculta[P] == letra:
+                adivinhando[P] = letra
+                Flag = 1
+            P += 1
+
+        if Flag == 0:
+            print(f'A letra {letra} não existe nessa palavra')
+            chances += 1
+
+        if adivinhando == oculta:
+            print('Você ganhou')
             break
-        # dizemos quantas chances ainda restam
-        # caso a pessoa conclua ela termina o jogo, caso não, a palavra aparece para ela ver o que falta
-        print("Você possui ", chances, " chances, continue tentando!")
-        if adivinhando != palavra:
-            print("Palavra em andamento: ", adivinhando)
-        else:
-            print("Boa! Você conseguiu! A palavra secreta era: ", palavra)
-            print('Parabens você ganhou')
-            #break
-            V = 1
+            
+        print(adivinhando)
+        print(f'Você tem {chances} vidas')
+    
+    if chances == 6:
+        print('Vocẽ perdeu todas as suas vidas')
 
-    if V == 1 or V == 0:
-        op = int(input('Você deseja continuar jogando? \n\n1 - Sim \n2 - Não \n'))
-    
-    if op == 2:
-        break
-    
-# colocar o turle (desenho criado esta no arquivo auxiliar.py)
+    op = int(input('Você deseja continuar jogando? \n\n1 - para continuar \n2 - para parar'))
+
+print('Obrigado por jogar')
